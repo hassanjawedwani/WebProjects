0772a04c08aefeb9378e980e6c50dee64c553fe9
firstRepo <hassanjawedwani@gmail.com>
2024-07-26 10:44:09 +0500

    pyramid function done

--- Shape Generator/script.js
+++ Shape Generator/script.js
@@ -1,8 +1,7 @@
 function createPyramid(ch, cnt) {
   let rows = "";
   for (let i = 1; i <= cnt; i++) {
-    const row =
-      " ".repeat(cnt - i) + ch.repeat(i + (i - 1)) + " ".repeat(cnt - i);
+    const row = " ".repeat(cnt - i) + ch.repeat(i + (i - 1)) + " ".repeat(cnt - i);
     rows += row + "\n";
   }
   return rows;
