5de79b36764cc74e6c985067ffbe49a06bc78f80
firstRepo <hassanjawedwani@gmail.com>
2024-08-02 13:17:21 +0500

    play-button functionality finished

--- Music Player/script.js
+++ Music Player/script.js
@@ -1,6 +1,78 @@
-import { allSongs } from "./songs.js";
-
 const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play-button");
+
+const allSongs = [
+  {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+  },
+  {
+    id: 1,
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
+  },
+  {
+    id: 2,
+    title: "Still Learning",
+    artist: "Quincy Larson",
+    duration: "3:51",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
+  },
+  {
+    id: 3,
+    title: "Cruising for a Musing",
+    artist: "Quincy Larson",
+    duration: "3:34",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cruising-for-a-musing.mp3",
+  },
+  {
+    id: 4,
+    title: "Never Not Favored",
+    artist: "Quincy Larson",
+    duration: "3:35",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/never-not-favored.mp3",
+  },
+  {
+    id: 5,
+    title: "From the Ground Up",
+    artist: "Quincy Larson",
+    duration: "3:12",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/from-the-ground-up.mp3",
+  },
+  {
+    id: 6,
+    title: "Walking on Air",
+    artist: "Quincy Larson",
+    duration: "3:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/walking-on-air.mp3",
+  },
+  {
+    id: 7,
+    title: "Can't Stop Me. Can't Even Slow Me Down.",
+    artist: "Quincy Larson",
+    duration: "3:52",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cant-stop-me-cant-even-slow-me-down.mp3",
+  },
+  {
+    id: 8,
+    title: "The Surest Way Out is Through",
+    artist: "Quincy Larson",
+    duration: "3:10",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/the-surest-way-out-is-through.mp3",
+  },
+  {
+    id: 9,
+    title: "Chasing That Feeling",
+    artist: "Quincy Larson",
+    duration: "2:43",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/chasing-that-feeling.mp3",
+  },
+];
 
 const userData = {
   songs: allSongs,
@@ -8,23 +80,28 @@ const userData = {
   currentDuration: 0,
 };
 
-const sortSongs = () => {
-  return userData?.songs.sort((a, b) => {
-    if (a.title < b.title) {
-      return -1
-    }
-    if (a.title > b.title) {
-      return 1
-    }
-    return 0;
-  });
-}
+const audio = new Audio();
+
+const playSong = (id) => {
+  const song = userData?.songs.find(song => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+  if (userData?.currentSong === null || userData?.currentSong.id !== id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.currentDuration;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+  audio.play();
+};
 
 const renderSongs = (songs) => {
-  const songsHTML = songs.map((song) => {
-    return `
+  const songsHTML = songs
+    .map((song) => {
+      return `
       <li id="song-${song.id}" class="playlist-song">
-        <button class="playlist-song-info">
+        <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -35,9 +112,29 @@ const renderSongs = (songs) => {
         </button> 
       </li>
     `;
-  }).join("");
+    })
+    .join("");
   playlistSongs.innerHTML = songsHTML;
 };
 
-renderSongs(sortSongs());
+playButton.addEventListener("click", () => {
+  if (userData?.currentSong) {
+    playSong(userData?.currentSong.id);
+  } else {
+    playSong(userData?.songs[0].id);
+  }
+});
 
+const sortSongs = () => {
+  return userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+renderSongs(sortSongs());
