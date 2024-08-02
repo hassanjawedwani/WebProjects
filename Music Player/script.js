9748e7782a92f0c74ad0503e78b522afdc46fe67
firstRepo <hassanjawedwani@gmail.com>
2024-08-02 21:40:51 +0500

    reset functionality remaining

--- Music Player/script.js
+++ Music Player/script.js
@@ -3,6 +3,7 @@ const playButton = document.getElementById("play-button");
 const pauseButton = document.getElementById("pause-button");
 const nextButton = document.getElementById("next-button");
 const previousButton = document.getElementById("previous-button");
+const shuffleButton = document.getElementById("shuffle");
 
 const allSongs = [
   {
@@ -90,8 +91,26 @@ const songHighlight = (song) => {
   const targetElement = document.getElementById(`song-${song.id}`);
   songElements.forEach(songEl => songEl.classList.remove("highlight"));
   targetElement.classList.add("highlight");
-
 }
+
+const setPlayerDisplay = (song) => {
+  const currentTitle = song.title;
+  const currentArtist = song.artist;
+  const songTitle = document.querySelector(".song-title")
+  const songArtist = document.querySelector(".singer-title");
+ songTitle.textContent =  currentTitle ? currentTitle : "";
+ songArtist.textContent =  currentArtist ? currentArtist : "";
+}
+
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.currentDuration = 0;
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+}
+
 
 const playSong = (id) => {
   const song = userData?.songs.find(song => song.id === id);
@@ -105,6 +124,7 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add("playing");
   songHighlight(song);
+  setPlayerDisplay(song);
   audio.play();
 };
 
@@ -134,6 +154,19 @@ const previousSong = () => {
   }
 }
 
+const pauseSong = () => {
+  userData.currentDuration = audio.currentTime;
+  playButton.classList.remove("playing");
+  audio.pause();
+};
+
+const deleteSong = (id) => {
+  userData.songs = userData?.songs.filter(song => song.id !== id);
+  renderSongs(userData?.songs);
+  pauseSong();
+}
+
+// reset function remaining
 
 
 const renderSongs = (songs) => {
@@ -146,7 +179,7 @@ const renderSongs = (songs) => {
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
         </button>
-        <button class="playlist-song-delete">
+        <button class="playlist-song-delete" onclick="deleteSong(${song.id})">
         <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
           <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
         </button> 
@@ -165,15 +198,13 @@ playButton.addEventListener("click", () => {
   }
 }); 
 
-pauseButton.addEventListener("click", () => {
-  userData.currentDuration = audio.currentTime;
-  playButton.classList.remove("playing");
-  audio.pause();
-})
+pauseButton.addEventListener("click", pauseSong);
 
 nextButton.addEventListener("click", nextSong);
 
 previousButton.addEventListener("click", previousSong);
+
+shuffleButton.addEventListener("click", shuffle);
 
 const sortSongs = () => {
   return userData?.songs.sort((a, b) => {
