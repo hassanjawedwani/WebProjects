63b39a20c2a85d3d15de1d640b7e4eeefd50061e
firstRepo <hassanjawedwani@gmail.com>
2024-08-11 12:23:03 +0500

    project completed

--- Binary Converter/script.js
+++ Binary Converter/script.js
@@ -3,16 +3,36 @@ const convertBtn = document.getElementById("convert-button");
 const output = document.getElementById("output");
 
 const decimalToBinary = (input) => {
-  if (input === 0) {
-    return "0";
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-  const remainders = [];
-  while (input > 0) {
-    remainders.push(input % 2);
-    input = Math.floor(input / 2);
-  }
-  return remainders.reverse().join("");
-}
+
+
+  // another method
+  // let binary = "";
+  // if (input === 0) {
+  //   binary = "0";
+  //   return;
+  // }
+  // while (input > 0) {
+  //   binary = input % 2 + binary;
+  //   input = Math.floor(input / 2);
+  // }
+  // return binary;
+
+  // another method
+  // if (input === 0) {
+  //   return "0";
+  // }
+  // const remainders = [];
+  // while (input > 0) {
+  //   remainders.push(input % 2);
+  //   input = Math.floor(input / 2);
+  // }
+  // return remainders.reverse().join("");
+};
 
 const getUserInput = () => {
   if (
