8ad5cc06724318634aba0e4e81ad3ba0be7b7718
firstRepo <hassanjawedwani@gmail.com>
2024-08-11 10:31:20 +0500

    binary Converter with one method completed

--- Binary Converter/script.js
+++ Binary Converter/script.js
@@ -2,6 +2,18 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-button");
 const output = document.getElementById("output");
 
+const decimalToBinary = (input) => {
+  if (input === 0) {
+    return "0";
+  }
+  const remainders = [];
+  while (input > 0) {
+    remainders.push(input % 2);
+    input = Math.floor(input / 2);
+  }
+  return remainders.reverse().join("");
+}
+
 const getUserInput = () => {
   if (
     !numberInput.value ||
@@ -11,6 +23,7 @@ const getUserInput = () => {
     alert("Enter a valid number greater than 0");
     return;
   }
+  output.innerText = decimalToBinary(parseInt(numberInput.value));
 };
 
 convertBtn.addEventListener("click", getUserInput);
