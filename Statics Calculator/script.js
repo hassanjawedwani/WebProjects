8e60ad5b8440e28de8f923670bd5a0883f24e79a
firstRepo <hassanjawedwani@gmail.com>
2024-08-18 02:39:06 +0500

    project completed mode remaining

--- Statics Calculator/script.js
+++ Statics Calculator/script.js
@@ -11,10 +11,46 @@ const getMedian = (unsortedArray) => {
   }
 };
 
+const getMode = (array) => {
+  // const counts = {};
+  // array.map(el => counts[el] = (counts[el] || 0) + 1);
+  // console.log(Object.values(counts));
+  // if (new Set(Object.values(counts)).size === 1) {
+  //   return null;
+  // }
+
+
+  return "need update"
+}
+
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+}
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const variance = array.reduce((acc, el) => {
+    const difference = mean - el;
+    const square = difference ** 2;
+    return acc + square
+  }, 0) / array.length
+  return variance;
+}
+
+const getStandardDeviation = (array) => {
+  return Math.sqrt(getVariance(array));
+}
+
 const calculate = () => {
   const value = document.getElementById("numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((num) => !isNaN(num));
   document.getElementById("mean").textContent = getMean(numbers);
   document.getElementById("median").textContent = getMedian(numbers);
+  document.getElementById("mode").textContent = getMode(numbers);
+  document.getElementById("range").textContent = getRange(numbers);
+  document.getElementById("variance").textContent = getVariance(numbers);
+  document.getElementById("standard-deviation").textContent = getStandardDeviation(numbers);
+
 };
+
