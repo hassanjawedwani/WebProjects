e4a4de96f7d2e609edbcda3257b17e430cdaee52
firstRepo <hassanjawedwani@gmail.com>
2024-08-24 23:44:39 +0500

    keep selected button functionality done

--- Roll Dice Game/script.js
+++ Roll Dice Game/script.js
@@ -4,8 +4,13 @@ const rollsText = document.querySelectorAll(".round-text span")[0];
 const roundText = document.querySelectorAll(".round-text span")[1];
 const radioInputs = document.querySelectorAll(".score-options input");
 const radioSpans = document.querySelectorAll(".score-options span");
+const keepAboveSelectedBtn = document.getElementById("keep-above-selected-btn");
+const scoreHistory = document.getElementById("score-history");
+const totalScore = document.getElementById("total-score");
 let diceArr = [];
 let rolls = 0;
+let round = 1;
+let score = 0;
 
 const rollDice = () => {
   diceArr = [];
@@ -45,10 +50,22 @@ const getHighestDuplicates = (arr) => {
   updateRadioOptions(5, 0);
 };
 
+const resetOptions = () => {
+  radioInputs.forEach(input => {
+    input.disabled = true;
+    input.checked = false;
+  })
+  radioSpans.forEach(span => {
+    span.innerHTML = "";
+  })
+}
+
+
 const rollDiceHandler = () => {
   if (rolls === 3) {
     alert("Kindly select one option before moving to next round");
   } else {
+    resetOptions();
     rollDice();
     rolls++;
     rollsText.textContent = rolls;
@@ -56,4 +73,36 @@ const rollDiceHandler = () => {
   }
 };
 
+const keepAboveSelectedHandler = (scoreValue, inputId) => {
+  const selectedInput = [...radioInputs].find(radio => radio.checked === true);
+  
+  if (selectedInput) {
+    score += Number(selectedInput.value);
+    scoreHistory.innerHTML += `<li>${selectedInput.id} : ${selectedInput.value}`;
+    totalScore.textContent = score;
+    round++;
+    rolls = 0;
+    roundText.textContent = round;
+    rollsText.textContent = rolls;
+    if (round > 6) {
+      alert(`your total score is ${score}, game over`);
+      resetOptions();
+      round = 1;
+      roundText.textContent = round;
+      rolls = 0;
+      rollsText.textContent = rolls;
+      score = 0;
+      totalScore.textContent = score;
+      dice.forEach(die => die.textContent = 0);
+      diceArr = [];
+      scoreHistory.innerHTML = ``;
+    }
+  } else {
+    alert("please roll and select an option before submitting");
+  }
+};
+
+
 rollDiceBtn.addEventListener("click", rollDiceHandler);
+keepAboveSelectedBtn.addEventListener("click", keepAboveSelectedHandler);
+resetOptions();
