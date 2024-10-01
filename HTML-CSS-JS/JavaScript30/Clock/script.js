d4085f0463f1d65845e3bc024b558aaf694e83c5
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-10-02 00:45:04 +0500

    project completed

--- HTML-CSS-JS/JavaScript30/Clock/script.js
+++ HTML-CSS-JS/JavaScript30/Clock/script.js
@@ -1,12 +1,19 @@
 const secondHand = document.querySelector('.second-hand');
+const minutesHand = document.querySelector('.minute-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
 
 function everySecond() {
   const date = new Date();
   const seconds = date.getSeconds();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds);
-
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  const hoursDegrees = ((hours / 60) * 360) + 90;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(everySecond, 1000);
