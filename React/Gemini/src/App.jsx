3cc7ec52ea325f4b06fc53d6491947facf5dfbb8
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-10-02 07:22:42 +0500

    navbar done

--- React/Gemini/src/App.jsx
+++ React/Gemini/src/App.jsx
@@ -1,9 +1,11 @@
+import Main from "./components/main/main";
 import Sidebar from "./components/sidebar/sidebar";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   )
 }
