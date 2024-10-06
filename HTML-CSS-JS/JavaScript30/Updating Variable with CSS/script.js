c7e652d010968a05b45a7605f1cca04be33bb1f2
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-10-07 00:23:47 +0500

    project completed

--- HTML-CSS-JS/JavaScript30/Updating Variable with CSS/script.js
+++ HTML-CSS-JS/JavaScript30/Updating Variable with CSS/script.js
@@ -1,7 +1,8 @@
 const inputs = document.querySelectorAll(".controls input");
 
 function handleInput() {
-  console.log(this.value);
+  const suffix = this.dataset.sizing || "";
+  console.log(document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix));
 }
 
 inputs.forEach((input) => input.addEventListener("click", handleInput));
