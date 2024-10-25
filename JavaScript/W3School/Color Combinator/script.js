53d3647c53304d27f0d12b92342565ff9343e7a8
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-10-25 16:16:18 +0500

    project completed

--- JavaScript/W3School/Color Combinator/script.js
+++ JavaScript/W3School/Color Combinator/script.js
@@ -1,2 +1,15 @@
-const inputs = document.querySelectorAll("control");
-console.log(inputs);
+document.addEventListener("DOMContentLoaded", () => {
+  const inputs = document.querySelectorAll(".input");
+  inputs.forEach(input => {
+    function updateInputText(e) {
+      document.querySelector(`.${this.id}-output`).textContent = e.target.value;
+
+      const redValue = Number(document.querySelector(`.red-output`).textContent);
+      const greenValue = Number(document.querySelector(`.green-output`).textContent);
+      const blueValue = Number(document.querySelector(`.blue-output`).textContent);
+      document.getElementById("combinated-color").style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+    }
+    input.addEventListener("input", updateInputText);
+  });
+
+})
