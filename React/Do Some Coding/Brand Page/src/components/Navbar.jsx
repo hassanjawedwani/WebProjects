9723fb5794fe9e2be154c37415b46bc99d0b073f
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2024-10-26 13:25:04 +0500

    Brand Page Header Done

---
+++ React/Do Some Coding/Brand Page/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import logo from '../assets/brand_logo.png';
+
+const Navbar = () => {
+  return (
+    <header className="max-w-7xl h-[72px] flex justify-between items-center mx-auto">
+      <img src={logo} alt="logo" className="w-[76px] h-[42.19px]"/>
+      <nav>
+        <ul className="flex gap-6">
+          <li><a href="">Menu</a></li>
+          <li><a href="">Location</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
+      <button className="px-4 py-[6px] font-inter font-semibold text-base text-white bg-customRed">login</button>
+    </header>
+  )
+}
+
+export default Navbar
