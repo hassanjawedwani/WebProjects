6ab51176255ab4de6c8fcc515708179fed383ce6
Hassan Jawed Wani <hassanjawedwani@gmail.com>
2025-01-02 19:58:30 +0500

    todo basic functionality done

--- JavaScript/JavaScript30/Todo App/script.js
+++ JavaScript/JavaScript30/Todo App/script.js
@@ -3,6 +3,13 @@ const inputs = document.querySelectorAll('input[type = "checkbox"]');
 inputs.forEach((input) => {
   input.addEventListener("change", () => {
     const inputs = document.querySelectorAll('input[type = "checkbox"]');
-    console.log(inputs);
+    inputs.forEach(input => {
+      const label = document.querySelector(`label[for="${input.id}"]`);
+      if (input.checked) {
+        label.style.textDecoration = "line-through"
+      } else {
+        label.style.textDecoration = "none";
+      }
+    });
   });
 });
