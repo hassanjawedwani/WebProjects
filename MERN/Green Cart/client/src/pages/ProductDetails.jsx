118a8ab85e655d01b4188e071e195ca4e6fb027f
HassanJawedWani <hassanjawedwani@gmail.com>
2025-05-03 09:56:27 +0500

    related product added to product detail page

--- MERN/Green Cart/client/src/pages/ProductDetails.jsx
+++ MERN/Green Cart/client/src/pages/ProductDetails.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router';
 import { useAppContext } from '../context/AppContext';
 import { assets } from '../assets/assets';
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const { products, currency, cartItems, addToCart, removeToCart } = useAppContext();
+  const { products, currency, cartItems, addToCart, removeToCart, navigate } = useAppContext();
   const [showcaseImageURL, setShowcaseImageURL] = useState("");
   const product = products.find(product => product._id === id);
-  console.log(id, product);
+  const [relatedProducts, setRelatedProducts] = useState(null);
+
+  useEffect(() => {
+    setRelatedProducts(products.filter(productTemp => productTemp.category === product.category));
+  }, [products]);
+
+  console.log(id, product, relatedProducts);
+
   return (
     <>
-      { product && (
+      {product && (
+        <div>
         <div className='my-10 max-h-6xl'>
           <p className='my-5'>Products / {product.category} <span className='text-primary-dull'>{product.name}</span></p>
           <div className='flex flex-col md:flex-row gap-10'>
@@ -52,7 +60,7 @@ const ProductDetails = () => {
                   <div className='text-sm text-primary border border-primary py-1 px-2 flex gap-2 shrink-0 w-20'>
                     <button type='button'className='w-6 ' onClick={() => { removeToCart(product._id) }}>-</button>
                     <span className='w-7 text-center'>{cartItems[product._id]}</span>
-                    <button type='button' className='w-6' onClick={() => addToCart(product._id)}>+</button>
+                      <button type='button' className='w-6' onClick={() => { addToCart(product._id);  } }>+</button>
                   </div>
                   :
                   (
@@ -62,8 +70,59 @@ const ProductDetails = () => {
                   )
                   
                 } 
-                <button className='bg-primary hover:bg-primary-dull text-white font-medium h-9 w-30'>Buy Now</button>
+                  <button className='bg-primary hover:bg-primary-dull text-white font-medium h-9 w-30' onClick={() => { addToCart(); navigate("/cart"); } }>Buy Now</button>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className='my-15'>
+            <h2 className='text-2xl md:text-3xl font-medium text-slate-700 pb-5'>Related Products</h2>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-10 '>
+              {relatedProducts.length > 0
+                ? (
+                relatedProducts.map((product, index) => (
+                  <div key={index} className='group rounded-lg flex flex-col justify-center p-4 border border-gray-200 gap-0.5'>
+                    <img src={product.image[0]} alt={product.name} className='w-24 h-24 transition-transform group-hover:scale-125 self-center' onClick={() => navigate(`/products/${product.category}/${product._id}`)}/>
+                    <p className='text-sm text-slate-400'>{product.category}</p>
+                    <h3 className='text-lg my-1 font-medium text-slate-700'>{product.name}</h3>
+                    <div className='flex gap-0.5'>
+                      <img src={assets.star_icon} alt="star icon" />
+                      <img src={assets.star_icon} alt="star icon" />
+                      <img src={assets.star_icon} alt="star icon" />
+                      <img src={assets.star_icon} alt="star icon" />
+                      <img src={assets.star_dull_icon} alt="star icon" />
+                      <span className='text-slate-400'>(4)</span>
+                    </div>
+                    <div className=' flex flex-row justify-between gap-2 mt-2 flex-wrap '>  {/*onClick={e => e.stopPropagation()}*/}
+                      <div className='flex items-end-safe gap-x-1'>
+                        <p className='font-medium text-primary'>{currency}{product.offerPrice}</p>
+                        <p className='line-through text-xs text-slate-400'>{currency}{product.price}</p>
+                      </div>
+                      {cartItems[product._id] 
+                        ?
+                        <div className='text-sm text-primary border border-primary py-1 px-2 flex gap-2 shrink-0 w-20'>
+                          <button type='button'className='w-6 ' onClick={() => { removeToCart(product._id) }}>-</button>
+                          <span className='w-7 text-center'>{cartItems[product._id]}</span>
+                          <button type='button' className='w-6' onClick={() => addToCart(product._id)}>+</button>
+                        </div>
+                        :
+                        (
+                          <button className='text-sm text-primary border border-primary py-1 px-2 flex gap-2 shrink-0 w-20 justify-center' onClick={() => { addToCart(product._id); }}>
+                            <img src={assets.cart_icon} alt="cart icon" />
+                            <span>Add</span>
+                          </button>
+                        )
+                        
+                      } 
+                      
+                      
+                    </div>
+                  </div>
+                )))
+                : (
+                  <h2>No products</h2>
+                )
+              }
             </div>
           </div>
         </div>
